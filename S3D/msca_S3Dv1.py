9daaac997f7b05a7439da8561ddad349a16e6954
JackY <yysco24@gmail.com>
2022-12-10 13:55:37 +0800

    对MSCA进行了修改，得到第三版本，删去了其中多余的srm网络。

--- S3D/msca_S3Dv1.py
+++ S3D/msca_S3Dv1.py
@@ -14,6 +14,63 @@ from new_model.iformer_3d import iFormerBlock_light
 from new_model.msca_3d import MSCAN_half
 from new_model.msca_3d import MSCAN
 
+class msca_S3D_SRM(nn.Module):
+    def __init__(self, num_class, SRM_net):
+        super(msca_S3D_SRM, self).__init__()
+
+        self.SRM_net = SRM_net
+        if SRM_net == 'yes':
+            input_channels = 3
+        else:
+            input_channels = 3
+
+        self.SRM = HPF_3()
+        self.base = nn.Sequential( # input:bs*(3/30) *20*224*224
+            SepConv3d(input_channels, 64, kernel_size=7, stride=2, padding=3),#out:bs*64*10*112*112
+            nn.MaxPool3d(kernel_size=(1,3,3), stride=(1,2,2), padding=(0,1,1)),#out:bs*64*10*56*56
+            MSCAN_half(64, 1),#out:bs*64*10*56*56
+
+            BasicConv3d(64, 64, kernel_size=1, stride=1),#out:bs*64*10*56*56
+            SepConv3d(64, 192, kernel_size=3, stride=1, padding=1),#out:bs*192*10*56*56
+            nn.MaxPool3d(kernel_size=(1,3,3), stride=(1,2,2), padding=(0,1,1)),#out:bs*192*10*28*28
+            iFormerBlock_light(192, 1/4, 1),#out:bs*192*10*28*28
+            iFormerBlock(192, 1/4, 1),#out:bs*192*10*28*28
+
+            BasicConv3d(192, 320, kernel_size=1, stride=1),#out:bs*320*10*28*28
+            # SepConv3d(128, 160, kernel_size=3, stride=1, padding=1),#out:bs*160*10*28*28
+            nn.MaxPool3d(kernel_size=(3,3,3), stride=(2,2,2), padding=(1,1,1)),#out:bs*320*5*14*14
+            iFormerBlock_light(320, 1/3, 3),#out:bs*320*10*28*28
+            iFormerBlock_light(320, 1/3, 3),#out:bs*320*10*28*28
+            iFormerBlock(320, 1/3, 3),#out:bs*320*10*28*28
+            iFormerBlock_light(320, 1/2, 3),#out:bs*320*10*28*28
+            iFormerBlock_light(320, 1/2, 3),#out:bs*320*10*28*28
+            iFormerBlock(320, 1/2, 3),#out:bs*320*10*28*28
+            iFormerBlock_light(320, 2/3, 3),#out:bs*320*10*28*28
+            iFormerBlock_light(320, 2/3, 3),#out:bs*320*10*28*28
+            iFormerBlock(320, 2/3, 3),#out:bs*320*10*28*28
+
+            # BasicConv3d(320, 384, kernel_size=1, stride=1),#out:bs*512*5*14*14
+            # SepConv3d(160, 256, kernel_size=3, stride=1, padding=1),#out:bs*256*5*14*14
+            nn.MaxPool3d(kernel_size=(2,2,2), stride=(2,2,2), padding=(0,0,0)),#out:bs*320*2*7*7
+            Mixed_5b(320),#out:bs*512*2*7*7
+            Mixed_5c(512)#out:bs*1024*2*7*7
+            
+        )
+        self.fc = nn.Sequential(nn.Conv3d(1024, num_class, kernel_size=1, stride=1, bias=True),)
+
+    def forward(self, x):
+        if self.SRM_net == 'yes':
+            y = x + self.SRM(x)
+        else:
+            y = x
+        y = self.base(y)
+        y = F.avg_pool3d(y, (2, y.size(3), y.size(4)), stride=1)
+        y = self.fc(y)
+        y = y.view(y.size(0), y.size(1), y.size(2))
+        logits = torch.mean(y, 2)
+
+        return logits
+
 class msca_S3D(nn.Module):
     def __init__(self, num_class, SRM_net):
         super(msca_S3D, self).__init__()
@@ -39,13 +96,13 @@ class msca_S3D(nn.Module):
             BasicConv3d(192, 320, kernel_size=1, stride=1),#out:bs*320*10*28*28
             # SepConv3d(128, 160, kernel_size=3, stride=1, padding=1),#out:bs*160*10*28*28
             nn.MaxPool3d(kernel_size=(3,3,3), stride=(2,2,2), padding=(1,1,1)),#out:bs*320*5*14*14
-            # iFormerBlock_light(320, 1/3, 3),#out:bs*128*5*14*14
+            iFormerBlock_light(320, 1/3, 3),#out:bs*128*5*14*14
             iFormerBlock_light(320, 1/3, 3),#out:bs*320*5*14*14
             iFormerBlock(320, 1/3, 3),#out:bs*320*5*14*14
-            # iFormerBlock_light(320, 1/2, 3),#out:bs*320*5*14*14
+            iFormerBlock_light(320, 1/2, 3),#out:bs*320*5*14*14
             iFormerBlock_light(320, 1/2, 3),#out:bs*320*5*14*14
             iFormerBlock(320, 1/2, 3),#out:bs*320*5*14*14
-            # iFormerBlock_light(320, 2/3, 3),#out:bs*320*5*14*14
+            iFormerBlock_light(320, 2/3, 3),#out:bs*320*5*14*14
             iFormerBlock_light(320, 2/3, 3),#out:bs*320*5*14*14
             iFormerBlock(320, 2/3, 3),#out:bs*320*5*14*14
 
